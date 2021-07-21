67204d8efd729d9b2634243306a9207fec34c14e
Ivan <thefullarcticfox@users.noreply.github.com>
2021-07-21 12:37:22 +0300

    hello world kernel module

---
+++ hello-kernel-module/main.c
@@ -0,0 +1,22 @@
+#include <linux/init.h>		// included for __init and __exit macros
+#include <linux/module.h>	// included for all kernel modules
+#include <linux/kernel.h>	// included for KERN_INFO
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("salec aka thefullarcticfox");
+MODULE_DESCRIPTION("A Simple Hello World module for little-penguin-1");
+MODULE_VERSION("0.01");
+
+static int __init hello_init(void)
+{
+	printk(KERN_INFO "Hello world!\n");
+	return 0;	// Non-zero return means that the module couldn't be loaded.
+}
+
+static void __exit hello_cleanup(void)
+{
+	printk(KERN_INFO "Cleaning up module.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_cleanup);
