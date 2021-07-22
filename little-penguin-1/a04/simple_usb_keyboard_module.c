344300df67ffd073bee7e0e32f3597bf379c8382
Ivan <thefullarcticfox@users.noreply.github.com>
2021-07-22 17:41:18 +0300

    testing a04

---
+++ little-penguin-1/a04/simple_usb_keyboard_module.c
@@ -0,0 +1,22 @@
+#include <linux/init.h>		// included for __init and __exit macros
+#include <linux/module.h>	// included for all kernel modules
+#include <linux/kernel.h>	// included for KERN_INFO
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("salec aka thefullarcticfox");
+MODULE_DESCRIPTION("A Simple USB keyboard module for little-penguin-1");
+MODULE_VERSION("0.01");
+
+static int __init usbkeyboard_module_init(void)
+{
+	printk(KERN_INFO "simple usb keyboard module init\n");
+	return 0;
+}
+
+static void __exit usbkeyboard_module_cleanup(void)
+{
+	printk(KERN_INFO "simple usb keyboard module cleanup\n");
+}
+
+module_init(usbkeyboard_module_init);
+module_exit(usbkeyboard_module_cleanup);
